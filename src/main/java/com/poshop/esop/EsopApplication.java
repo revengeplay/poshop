66e2bfee04ec0979feff27d40a9370f69c969e40
web <tmdghks0914@naver.com>
2022-09-27 23:17:26 +0900

    main

---
+++ src/main/java/com/poshop/esop/EsopApplication.java
@@ -0,0 +1,13 @@
+package com.poshop.esop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poshop/esop/EsopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshop.esop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
